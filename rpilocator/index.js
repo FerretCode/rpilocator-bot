c6eda8b183e5957892ea6ed53bf7b6e20f00fcfe
ferretcode <49322226+FerretCode@users.noreply.github.com>
2022-12-13 10:05:48 -0800

    delete unused rpilocator directory

--- rpilocator/index.js
+++
@@ -1,8 +0,0 @@
-// install express with `npm install express` 
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-// export 'app'
-module.exports = app
